8701603a6d4e6e539ce45815ce7687a088eaf343
Henning Beyer <bubens@unpunk.de>
2019-12-20 21:59:57 +0100

    Move to terser

--- make.sh
+++ make.sh
@@ -10,9 +10,9 @@ echo "Making Sorting"
 echo "Step 1: Make"
 elm make --optimize --output=$TMP_OUT_ELM $SOURCE
 echo "Step 2: Compress"
-uglifyjs $TMP_OUT_ELM --output=$TMP_OUT_UJS --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe'
+terser $TMP_OUT_ELM --output=$TMP_OUT_UJS --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe'
 echo "Step 3: Mangle"
-uglifyjs $TMP_OUT_UJS --output=$OUTPUT --mangle
+terser $TMP_OUT_UJS --output=$OUTPUT --mangle
 echo "Results:"
 echo "   Orginal size: $(cat $SOURCE | wc -c) bytes ($SOURCE)"
 echo "   Compiled size: $(cat $TMP_OUT_ELM | wc -c) bytes ($TMP_OUT_ELM)"
